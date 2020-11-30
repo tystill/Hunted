ceba7339c21696023c4a1ca4df31479a3e9ca553
tystill <trstill@ucsb.edu>
2020-11-29 18:08:29 -0800

    implemented basic loop
    basic tracking and maze and player movement implemented. Maze revised several times to make it a bit easier and technically possible to pass the zombie without it getting within 4 meters of you

---
+++ Hunted/Assets/Scripts/Enemy.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+
+    public float DeathDistance = 0.5f;
+    public float DistanceAway;
+    public Transform thisObject;
+    public Transform target;
+    private NavMeshAgent navComponent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        navComponent = gameObject.GetComponent<NavMeshAgent>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        float dist = Vector3.Distance(target.position, transform.position);
+
+        if (target)
+        {
+            navComponent.SetDestination(target.position);
+        }
+        else
+        {
+            if(target == null)
+            {
+                target = gameObject.GetComponent<Transform>();
+            }
+            else
+            {
+                target = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+        }
+
+        if(dist <= DeathDistance)
+        {
+            //kill and lose life
+        }
+
+
+
+        
+    }
+}
---
+++ Hunted/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController controller;
+
+    private float speed = 4f;
+    private Vector3 move;
+
+    private float gravity = -9.81f;
+    private Vector3 velocity;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            speed = 6f;
+        }
+        else
+        {
+            speed = 4f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+
+}
